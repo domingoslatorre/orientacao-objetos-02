4993e85879e256689de568466eadfdc6c44ee6dc
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-03-07 19:08:30 -0300

    Criado classe retangulo

---
+++ src/main/java/exercicios/Retangulo.java
@@ -0,0 +1,38 @@
+package exercicios;
+
+public class Retangulo {
+    private Double base;
+    private Double altura;
+
+    public Retangulo(Double base, Double altura) {
+        if ( base > 0.0 && altura > 0.0 ) {
+            this.base = base;
+            this.altura = altura;
+        }
+    }
+
+    public Double calcularArea() {
+        return base * altura;
+    }
+
+    public Double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+
+    public void setBase(Double base) throws IllegalAccessException {
+        if ( base <= 0.0 )
+            throw new IllegalArgumentException("Valor deve ser maior que zero.");
+        this.base = base;
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public void setAltura(Double base) throws IllegalAccessException {
+        if ( altura <= 0.0 )
+            throw new IllegalArgumentException("Valor deve ser maior que zero.");
+        this.altura = altura;
+    }
+
+}
